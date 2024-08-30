0cf059b4e3bf0ffa617897ac57a96ef42e3a578d
Vitor <vitorricardosilvabarbosa@gmail.com>
2024-08-30 16:21:43 -0300

    Manipulação dos objetos definidos

--- TESTE/src/Main.java
+++ TESTE/src/Main.java
@@ -2,21 +2,35 @@ import Pessoa.Estagiario;
 import Pessoa.Funcionario;
 import Pessoa.Gestor;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	public static void main( String[] args ) {
-		Gestor gestor = new Gestor( "Marciano","Gestor",true );
-		Funcionario funcionario = new Funcionario( "Kleber","Cadastrador",true );
-		Estagiario estagiario = new Estagiario( "Jovem","Leitor de crédito",false ,true);
+		Gestor gestor = new Gestor( );
+		gestor.setNome( "Gilberto" );
+		gestor.setFuncao( "Gerente" );
+		gestor.setTrabalhando( true );
 
-		gestor.verificaStatusFuncionario( funcionario );
-		gestor.verificaStatusFuncionario( estagiario );
+		Funcionario funcionario = new Funcionario( );
+		funcionario.setNome( "Fabio" );
+		funcionario.setFuncao( "Facilitador" );
+		funcionario.setTrabalhando( false );
 
-		gestor.realizarOperacaoNoBanco();
+		Estagiario estagiario = new Estagiario( );
+		estagiario.setNome( "Enrique" );
+		estagiario.setFuncao( "Entregador" );
+		estagiario.setTrabalhando( true );
+
+		gestor.realizarOperacaoNoBanco( );
+		gestor.realizarOperacaoNoBanco( funcionario );
+		gestor.realizarOperacaoNoBanco( estagiario );
+		gestor.realizarOperacaoNoBanco( 240f, funcionario );
+		System.out.println( "-------------------" );
 		funcionario.realizarOperacaoNoBanco();
+		funcionario.realizarOperacaoNoBanco( 25 );
+		funcionario.realizarOperacaoNoBanco( 53.5f );
+		System.out.println( "-------------------" );
 		estagiario.realizarOperacaoNoBanco();
-
+		estagiario.realizarOperacaoNoBanco( 33.3f );
+		estagiario.realizarOperacaoBanco( false );
 
 		System.out.println( gestor.toString() );
 		System.out.println( funcionario.toString() );
